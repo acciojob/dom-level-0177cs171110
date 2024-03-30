af2ce5df992de6314fe18c8ec7e4ddee1b4eed76
Rahul Kumar <90969936+0177cs171110@users.noreply.github.com>
2024-03-30 14:33:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+window.onload = function() {
+  const targetElement = document.getElementById('level');
+  let domLevel = 0;
+  
+  let currentElement = targetElement;
+  while (currentElement !== document.documentElement) {
+    currentElement = currentElement.parentElement;
+    domLevel++;
+  }
+
+  console.log(`The level of the element is: ${domLevel}`);
+};
